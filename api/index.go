fcbff50727ea796bb75c1e037e78243490185f5f
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-09-08 02:12:05 +0700

    feat: add entrypoint

---
+++ api/index.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	app *gin.Engine
+)
+
+func myRoute(r *gin.RouterGroup) {
+	r.GET("/test", func(c *gin.Context) {
+		c.String(http.StatusOK, "API ready to use!")
+	})
+}
+
+func init() {
+	app = gin.New()
+	r := app.Group("/api")
+	myRoute(r)
+}
+
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
